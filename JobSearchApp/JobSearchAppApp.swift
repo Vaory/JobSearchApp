29375a841da783386ab2df5e9beb14aaea168bbe
Mikhail Demichev <demichev.mi@gmail.com>
2024-04-04 14:16:57 +0700

    Initial Commit

---
+++ JobSearchApp/JobSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchAppApp.swift
+//  JobSearchApp
+//
+//  Created by Mikhail Demichev on 04.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
